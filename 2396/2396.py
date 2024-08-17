0bd0fbdae5eb115adfaf23086d7795451f9ee7e3
vikixx13 <moonlightdawn118@gmail.com>
2024-08-17 16:00:02 +0530

    .

---
+++ 2396/2396.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        # for i in range(2, n - 1):
+        #     base_n_representation = []
+        #     number = n  
+        #     while number > 0:
+        #         base_n_representation.append(number % i)
+        #         number = number // i
+            
+        #     if base_n_representation != base_n_representation[::-1]:
+        #         return False
+        return False
