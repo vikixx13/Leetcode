fa1971f857054ee1327131f5535475f10167be63
vikixx13 <moonlightdawn118@gmail.com>
2024-08-26 06:27:34 +0530

    .

--- 217/217.py
+++ 217/217.py
@@ -1,5 +1,6 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        #return len(set(nums)) != len(nums)
         num=set()
         for n in nums:
             if n in num:
