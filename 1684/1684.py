a26b36716cccdcdc9e7eb3fb14adc07cd8b60c8d
vikixx13 <moonlightdawn118@gmail.com>
2024-09-12 22:04:37 +0530

    .

---
+++ 1684/1684.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        allowed = set(allowed)
+        count = 0
+        for word in words:
+            if all(c in allowed for c in word):
+                count += 1
+
+        return count
